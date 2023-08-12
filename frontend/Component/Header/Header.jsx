19710d557ed660e742018b9ad4f94ede80fcd394
Madhav <guptamadhav2828@gmail.com>
2023-08-12 16:37:55 +0530

    made some ui changes to header

--- frontend/Component/Header/Header.jsx
+++ frontend/Component/Header/Header.jsx
@@ -4,6 +4,8 @@ import { HStack , Heading , Flex  , Spacer , Box , Menu , MenuButton ,MenuItem ,
 import Link from 'next/link';
 import Style from "./Header.module.css"
 import { ConnectWallet  } from '@thirdweb-dev/react';
+import logo from "../../logo.png"
+import Image from 'next/image'
 
 
 const Header = () => {
@@ -13,15 +15,19 @@ const Header = () => {
   return (
     <>
 
-    <Flex minWidth='max-content' alignItems='around' p={4} gap='2'  style={{paddingBottom:'10rem' , background: "linear-gradient(135deg, #426F4E 0%, #05101A 100%)"  , color:"rgba(255, 255, 255, 0.90)"}} >
-    <Box p='2'>
-      <Heading size='md' style={{ fontWeight:'800',fontSize:"2rem" , padding : "1rem 2rem" , color:"#50A838"}} >Poply</Heading>
+    
+
+    <Flex minWidth='max-content' alignItems='around' p={4} gap='2'  style={{background: "linear-gradient(135deg, #426F4E 0%, #05101A 100%)"  , color:"rgba(255, 255, 255, 0.90)"}} >
+    <Box p='2' style={{marginLeft:'1rem'}}>
+    <Image  priority src={logo} style={{height:'2rem' , width:'2rem' , display:'inline'   , marginBottom:'1rem'}} />
+      <Heading size='md' style={{ fontWeight:'900',fontSize:"2rem" , padding : "1rem 2rem 1rem 0.6rem" ,display:'inline-block' , color:"#50A838"}} >Poply</Heading>
+
     </Box>
     {/* <Spacer /> */}
    <HStack>
    <Menu>
-   <MenuButton style={{marginLeft:'2rem' , color:"rgba(255, 255, 255, 0.90)" , padding:'0.5rem 1rem'}} className={Style.a} fontWeight="500" fontSize="lg" _hover={{ textDecoration: 'underline' , cursor:'pointer'}}>
-          Navigation
+   <MenuButton style={{marginLeft:'2rem' , fontSize:'1.6rem' , color:"#50A838" , padding:'0.5rem 1rem'}} className={Style.a} fontWeight="500" fontSize="lg" _hover={{ textDecoration: 'underline' , cursor:'pointer'}}>
+          Explore
           </MenuButton >
           <MenuList style={{backgroundColor:"#426F4E"}}>
             <MenuItem style={{fontSize:'1.2rem' , color:"rgba(255, 255, 255, 0.90)" , backgroundColor:"#426F4E"}}><Link  href={'/mintNFT'} >Mint AIFT</Link></MenuItem>
@@ -35,6 +41,8 @@ const Header = () => {
    <ConnectWallet theme="dark" btnTitle="Login" className={Style.button} />
 
   </Flex>
+
+  <div className={Style.thinwhiteborder} ></div>
 
 
     </>
