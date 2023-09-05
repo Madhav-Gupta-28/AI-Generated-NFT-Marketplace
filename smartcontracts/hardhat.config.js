079edb8fcc41b7ef9ec44a0e536720a1a536fbfd
Madhav <guptamadhav2828@gmail.com>
2023-09-05 22:20:15 +0530

    deploy contracts to celo testnet

--- smartcontracts/hardhat.config.js
+++ smartcontracts/hardhat.config.js
@@ -14,6 +14,17 @@ module.exports = {
       gasPrice: 8000000000,
       allowUnlimitedContractSize: true
     },
+
+    alfajores: {
+      // can be replaced with the RPC url of your choice.
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: [
+          "78d217cddc344385c3f5555253b95fcf119c1e2c5216a12e8a8aeec2dc03c52f"
+      ],
+      gas: "auto",
+      gasPrice: 8000000000,
+      allowUnlimitedContractSize: true
+  },
   
   }
 };
